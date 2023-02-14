8ef8df7cc7956ee4088c7f4b48fbb64711edce83
un4kn0wl3z <admin@unknownclub.net>
2023-02-14 14:15:48 +0700

    feat: init with bugs

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+fn main() {
+    let server = Server::new("127.0.0.1:8080");
+    server.run();
+}
+
+struct Server {
+    addr: String,    
+}
+
+impl Server {
+    fn new(addr: String) -> Self { // Associate function for create new instance of Server struct
+        Self {
+            addr
+        }
+    }
+
+    fn run(self) { // Method of server instance
+
+    }
+}

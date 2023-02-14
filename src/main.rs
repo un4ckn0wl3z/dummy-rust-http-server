21b13cffc75e39e40dfceded07b7f31ce06aca50
un4kn0wl3z <admin@unknownclub.net>
2023-02-14 15:12:39 +0700

    feat: change code base to modules

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,51 @@
 fn main() {
-    let server = Server::new("127.0.0.1:8080".to_string());
+    let server = server::Server::new("127.0.0.1:8080".to_string());
     server.run();
 }
 
-struct Server {
-    addr: String,    
+mod server {
+    pub struct Server {
+        addr: String,    
+    }
+    
+    impl Server {
+        pub fn new(addr: String) -> Self { // Associate function for create new instance of Server struct
+            Self {
+                addr
+            }
+        }
+    
+        pub fn run(self) { // Method of server instance
+            println!("Listening on {}", self.addr)
+        }
+    }
 }
 
-impl Server {
-    fn new(addr: String) -> Self { // Associate function for create new instance of Server struct
-        Self {
-            addr
+
+mod http {
+    mod request {
+        struct Request {
+            path: String,
+            query_string: Option<String>,
+            method: super::method::Method,
         }
     }
 
-    fn run(self) { // Method of server instance
-        println!("Listening on {}", self.addr)
+    mod method {
+        pub enum Method {
+            GET,
+            POST,
+            PUT,
+            HEAD,
+            CONNECT,
+            OPTIONS,
+            TRACE,
+            PATCH,
+            DELETE
+        }
     }
+
+    
+
 }
 
-struct Request {
-    path: String,
-    query_string: Option<String>,
-    method: Method,
-}
-
-enum Method {
-    GET,
-    POST,
-    PUT,
-    HEAD,
-    CONNECT,
-    OPTIONS,
-    TRACE,
-    PATCH,
-    DELETE
-}
